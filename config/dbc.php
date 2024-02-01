c01d15f572cefee97d00f7091814f65e60030eef
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-01 14:38:19 +0500

    Add files via upload

---
+++ config/dbc.php
@@ -0,0 +1,7 @@
+<?php 
+	define('DB_HOST', '127.0.0.1');
+	define('DB_USER', 'u453504845_07znr');
+	define('DB_PASSWORD', 'IflRtBjRNE');
+	define('DB_NAME', 'u453504845_WzqsH');
+	$connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+ ?>
