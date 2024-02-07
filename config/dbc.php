43cd5b2c0c871484c959a08f6b0f191b726b9521
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-07 20:07:10 +0500

    Update dbc.php

--- config/dbc.php
+++ config/dbc.php
@@ -1,7 +1,5 @@
 <?php 
-	define('DB_HOST', '127.0.0.1');
-	define('DB_USER', 'u453504845_07znr');
-	define('DB_PASSWORD', 'IflRtBjRNE');
-	define('DB_NAME', 'u453504845_WzqsH');
-	$connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	$con = mysqli_connect('localhost','u453504845_lj','selfmadeu12@@AA');
+	mysqli_select_db($con, 'u453504845_lj');
+
  ?>
